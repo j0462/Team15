2d1129510d43c6dccbd14cd6f760c52529800da1
LSH <k01094060462@gmail.com>
2024-05-02 18:06:07 +0900

    개인main

--- RealMain.java
+++ RealMain.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class RealMain {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         try {
